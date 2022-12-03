e66490704456d1f158f848020b13a63d05a89204
Felipe <felipedossantosflor@gmail.com>
2022-12-02 21:37:42 -0300

    @FelipeFlor [Task: Cap.02 Lab.02]

---
+++ Capitulo02.Lab02/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Lab02
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a quantidade de dígitos da senha: ");
+            int digitos = Convert.ToInt32(Console.ReadLine());
+
+            if(digitos < 4 || digitos > 10 || digitos %2 != 0)
+            {
+                Console.WriteLine($"O valor {digitos} é" +
+                    $"inválido de acordo com as regras.");
+                Console.ReadKey();
+                return;
+            }
+
+            string senha = "";
+
+            //objeto a partir do qual os valores aleatórios serão gerados
+            var rnd = new Random();
+
+            for (int i = 0; i < digitos; i++)
+            {
+                //valor aleatório entre 0 e 9
+                int numero = rnd.Next(0, 9);
+                senha += numero;
+            }
+            Console.WriteLine($"Senha gerada: {senha}");
+            Console.ReadKey();
+        }
+    }
+}
