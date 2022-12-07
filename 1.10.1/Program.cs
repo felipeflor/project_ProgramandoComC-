98c811e7d1a4f99281b43a14e2a9ae3be373b987
Felipe <felipedossantosflor@gmail.com>
2022-12-07 10:27:20 -0300

    @FelipeFlor [Task: 1.10.1]

---
+++ 1.10.1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace OperadoresAtribuicao{
+    public class Atribuicao
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            Console.WriteLine($"O valor de A é: {a}");
+
+            int b = a;
+            Console.WriteLine($"O valor de B é: {b}");
+
+            a = 5;
+            Console.WriteLine($"Novo valor de A é: {a}");
+            Console.WriteLine($"Valor de B: {b}");
+
+            Console.ReadKey();
+        }
+    }
+
+}
