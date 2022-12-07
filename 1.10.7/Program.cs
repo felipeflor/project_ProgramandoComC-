787adbfce63a77d22ecadc0ff5776a35b3b60159
Felipe <felipedossantosflor@gmail.com>
2022-12-07 16:06:15 -0300

    @FelipeFlor [Task: 1.10.7 e 8]

---
+++ 1.10.7/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace OperadorTernario
+{
+    public class OperadorTernario
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            numero = 10;
+            Console.WriteLine(numero + " é " + 
+                (numero % 2 == 0 ? "par" : "impar"));
+            Console.WriteLine(new string('-', 20));
+
+            numero = 15;
+            Console.WriteLine(numero + " é " +
+                (numero % 2 == 0 ? "par" : "impar"));
+            Console.WriteLine(new string('-', 20));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 1.10.8/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace CoalescenciaNula
+{
+    public class CoalescenciaNula
+    {
+        static void Main(string[] args)
+        {
+            string curso = null;
+            string texto = curso ?? "Curso não informado";
+
+            Console.WriteLine(texto);
+            Console.ReadKey();
+
+            List<string> cursos = null;
+            (cursos ??= new List<string>()).Add("C#");
+            Console.WriteLine(string.Join(" ", cursos));
+
+            Console.ReadKey();
+        }
+    }
+}
