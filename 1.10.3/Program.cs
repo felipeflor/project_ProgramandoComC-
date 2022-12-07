63aaa022b8f1f77b573418852b82a6e0f9e0ebcb
Felipe <felipedossantosflor@gmail.com>
2022-12-07 11:14:49 -0300

    @FelipeFlor [Task: 1.10.3]

---
+++ 1.10.3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace AtribuicaoReduzida
+{
+    public class AtribuicaoReduzida
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            Console.WriteLine("Valor de X: " + x.ToString());
+
+            //Equivale a x=x-3;
+            x -= 3;
+
+            Console.WriteLine($"Novo valor de X: {x}");
+            Console.ReadKey();
+        }
+    }
+}
