965602dcb5524e6b5ce237aedbaa6f8bb800e793
Felipe <felipedossantosflor@gmail.com>
2022-12-07 20:14:20 -0300

    @FelipeFlor [Task: Sobrecarga Construtores]

---
+++ SobrecargaConstrutores/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SobrecargaConstrutores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa p1 = new Pessoa(100, 1.88);
+            p1.Nome = "Juan";
+            p1.Idade = 46;
+            Console.WriteLine(p1.Altura);
+            Console.WriteLine(p1.Peso);
+            Console.WriteLine(p1.Nome);
+
+
+        }
+    }
+}
---
+++ SobrecargaConstrutores/SobrecargaConstrutores.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SobrecargaConstrutores
+{
+    //Exercicios pg. 188 e 189
+    internal class Pessoa
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public double Peso { get; set; }
+        public double Altura { get; set; }
+
+        public Pessoa(double peso, double altura)
+        {
+            this.Peso = peso;
+            this.Altura = altura;
+        }
+
+        public Pessoa(string nome, double peso, double altura)
+        {
+            this.Nome = nome;
+            this.Peso = peso;
+            this.Altura = altura;
+        }
+    }
+}
