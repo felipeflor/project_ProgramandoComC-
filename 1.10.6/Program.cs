09acb987612373cd1d3f1368accc120fdc4906c1
Felipe <felipedossantosflor@gmail.com>
2022-12-07 14:10:07 -0300

    @FelipeFlor [Task: 1.10.6]

---
+++ 1.10.6/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace OperadoresLogicos
+{
+    public class OperadoresLogicos
+    {
+        static void Main(string[] args)
+        {
+            int a = 30, b = 40, c = 50, d = 60;
+
+            Console.WriteLine($"a: {a}");
+            Console.WriteLine($"b: {b}");
+            Console.WriteLine($"c: {c}");
+            Console.WriteLine($"d: {d}");
+            Console.WriteLine(new string('-', 20));
+
+            Console.WriteLine("a < b || c == 50-- > " +(a < b || c == 50));
+            Console.WriteLine("a < b || c == d-- > " +(a < b || c == d));
+            Console.WriteLine("a > b || c == 50-- > " +(a > b || c == 50));
+            Console.WriteLine("a > b || c == d-- > " +(a > b || c == d));
+            Console.WriteLine(new string('-', 20));
+
+            Console.WriteLine("a < b && c == 50-- > " +(a < b && c == 50));
+            Console.WriteLine("a < b && c == d-- > " +(a < b && c == d));
+            Console.WriteLine("a > b && c == 50-- > " +(a > b && c == 50));
+            Console.WriteLine("a > b && c == d-- > " +(a > b && c == d));
+            Console.WriteLine(new string('-', 20));
+
+            Console.ReadKey();
+        }
+    }
+}
