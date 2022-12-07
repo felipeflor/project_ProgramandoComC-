fa32fb3e0acac1d3f6bfc1bb388ac07395f38b3a
Felipe <felipedossantosflor@gmail.com>
2022-12-07 16:35:55 -0300

    @FelipeFlor [Task: 1.12.1]

---
+++ 1.12.1/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace StringFormat
+{
+    public class StringFormat
+    {
+        static void Main(string[] args)
+        {
+            string produto = "caneta";
+            decimal preco = 6.32m;
+            Console.WriteLine(string.Format("Produto: {0}\nPreco: {1}",
+                produto, preco));
+
+            //Formato monetário com duas casas pós vírgula
+            //Respeita as configurações regionais do Windows
+            string produto2 = "caneta";
+            decimal preco2 = 6.32m;
+            Console.WriteLine(string.Format("Produto: {0}\nPreço: {1:c2}", produto, preco));
+
+            Console.ReadKey();
+        }
+    }
+}
