e95ec315b93a0179f9e4b9576f74df0d5fc7b31f
Felipe <felipedossantosflor@gmail.com>
2022-12-07 13:12:09 -0300

    @FelipeFlor [Task: 1.10.4]

---
+++ 1.10.4/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace IncrementoDecremento
+{
+    public class IncrementoDecremento
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            Console.WriteLine("Exemplo Pré-Incremental");
+            Console.WriteLine("Valor de A: " + a);
+            Console.WriteLine("2 + ++a = " + (2 + ++a));
+            Console.WriteLine("Valor de A: " + a);
+            Console.WriteLine(new string('-', 50));
+
+            a = 5;
+            Console.WriteLine("Exemplo Pós-Incremental");
+            Console.WriteLine("Valor de A: " + a);
+            Console.WriteLine("2 + a++ = " + (2 + ++a));
+            Console.WriteLine("Valor de A: " + a);
+            Console.WriteLine(new string('-', 50));
+
+            a = 5;
+            Console.WriteLine("Exemplo Pré-Decremental");
+            Console.WriteLine("Valor de A: " + a);
+            Console.WriteLine("2 + --a = " + (2 + --a));
+            Console.WriteLine("Valor de A: " + a);
+            Console.WriteLine(new string('#', 50));
+
+            a = 5;
+            Console.WriteLine("Exemplo Pós-Decremental");
+            Console.WriteLine("Valor de A: " + a);
+            Console.WriteLine("2 + a-- = " + (2 + a--));
+            Console.WriteLine("Valor de A: " + a);
+            Console.WriteLine(new string('#', 50));
+
+            Console.ReadKey();
+
+        }
+    }
+}
